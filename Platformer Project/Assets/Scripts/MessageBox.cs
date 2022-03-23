5d5456d98811fb7ec09bf8b75e37b898d9e5d2c5
Kondrashin-Egor <77448776+Kondrashin-Egor@users.noreply.github.com>
2022-03-23 19:42:15 +0300

    Nine.2

---
+++ Platformer Project/Assets/Scripts/MessageBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class MessageBox : MonoBehaviour {
+
+    private static MessageBox instance;
+
+    public GameObject Template;
+
+	// Use this for initialization
+	void Awake () {
+        instance = this;
+    }
+
+    public static void ShowMassage(string text)
+    {
+        GameObject massageBox = Instantiate(instance.Template);
+
+        Transform panel = massageBox.transform.Find("Panel");
+
+        Text massageBoxText = panel.Find("Text").GetComponent<Text>();
+        massageBoxText.text = text;
+
+        Button yes = panel.Find("Yes").GetComponent<Button>();
+
+        yes.onClick.AddListener(() =>
+        {
+            Destroy(massageBox);
+        });
+    }
+}
