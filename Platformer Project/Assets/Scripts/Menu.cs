8e1177001b6f69d0151ad5c622b0c53e86ce7f71
Kondrashin-Egor <77448776+Kondrashin-Egor@users.noreply.github.com>
2022-03-30 13:52:36 +0300

    Ten

---
+++ Platformer Project/Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Game Over");
+        Application.Quit();
+    }
+}
---
+++ Platformer Project/Assets/Scripts/Portals.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class Portals : MonoBehaviour
+{
+    private static Portals instance;
+
+    public GameObject Template;
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject endBox = Instantiate(instance.Template);
+
+            Transform panel = endBox.transform.Find("Panel");
+
+            Button yes = panel.Find("Yes").GetComponent<Button>();
+
+            yes.onClick.AddListener(() =>
+            {
+                Destroy(endBox);
+                SceneManager.LoadScene(1);
+            });
+        }
+    }
+}
